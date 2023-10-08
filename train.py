f4479b256a3dcf37d6de870bd198e2da1012719f
byounng <byounng@outlook.com>
2023-10-08 18:51:38 +0800

    "dont push"

--- train.py
+++ train.py
@@ -1,2 +1,9 @@
 from model.mem_basic_network import ttm
 from utils.log import logger
+from config import Config
+configs = Config.getInstance()["train"]
+log_writer=logger(configs["name"])
+
+#data
+data=zip(1,2)
+
