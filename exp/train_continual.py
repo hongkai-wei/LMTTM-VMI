48cb68252f931256f94f6045b63cbb5057b75223
WHKai666666 <1259632828@qq.com>
2023-11-01 13:14:26 +0800

    optimize AddErase

--- exp/train_continual.py
+++ exp/train_continual.py
@@ -38,8 +38,10 @@ data_val = get_dataloader("val",config=config,download=False, transform=transfor
 torch.manual_seed(0)
 
 def init_weights(m):
-    if isinstance(m, nn.Conv1d) or isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv3d) or isinstance(m, nn.Linear):
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv1d) or isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv3d):
         nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
 
 def train():
     
--- train.py
+++ train.py
@@ -38,8 +38,10 @@ seed = 0
 torch.manual_seed(seed)
 
 def init_weights(m):
-    if isinstance(m, nn.Conv1d) or isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv3d) or isinstance(m, nn.Linear) :
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv1d) or isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv3d):
         nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
 
 def train():
     
